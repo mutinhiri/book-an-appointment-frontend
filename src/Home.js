eda32d45657e2be5d8bc4a6074c110a54b40fe10
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-04-04 21:03:18 +0100

    Setup react router dom

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SideBar from './components/SideBar';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Appointment from './components/appointments/Appointments';
+import AppointmentForm from './components/appointments/AppointmentForm';
+import './App.css';
+
+const Home = () => (
+
+  <BrowserRouter>
+    <SideBar />
+    <div className="content">
+      <Header />
+      <div className="home-content">
+        <Routes>
+          <Route path="/appointments" element={<Appointment />} />
+          <Route path="/appointment-form" element={<AppointmentForm />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
+
+export default Home;
---
+++ src/components/appointments/AppointmentForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppointmentForm = () => (
+  <h2>Welcome to appointment form homepage</h2>
+);
+
+export default AppointmentForm;
