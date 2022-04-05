cc7920c1f4e4ca349f764d1d6141bca0576aac80
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-04-05 18:02:02 +0100

    Update appointment component to fetch appointments from backend

--- src/api/BaseURL.js
+++ src/api/BaseURL.js
@@ -1,4 +1,4 @@
-const Development = 'http://localhost:8080/api/v1';
+const Development = 'http://127.0.0.1:8080/api/v1/';
 // const Production = '';
 
 const BaseUrl = Development;
--- src/components/appointments/Appointments.js
+++ src/components/appointments/Appointments.js
@@ -1,7 +1,52 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import appointmentAPI from '../../api/appointmentAPI';
 
-const Appointment = () => (
-  <h2>Welcome to appointment homepage</h2>
-);
+const Appointment = () => {
+  const dispatch = useDispatch();
+  const appointments = useSelector((state) => state.appointments.appointments) || [];
+
+  useEffect(() => {
+    dispatch(appointmentAPI());
+  }, []);
+
+  return (
+    <div className="appointment-page">
+      {appointments.length > 0 && (
+        <div className="appointment">
+          <h2>Appointments</h2>
+          <div className="appointment-table">
+            <table>
+              <thead>
+                <tr>
+                  <th>S/N</th>
+                  <th>Name</th>
+                  <th>Location</th>
+                  <th>Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {appointments.map((appointment, index) => (
+                  <tr key={index}>
+                    <th>{index + 1}</th>
+                    <td>{appointment[1].doctor}</td>
+                    <td>{appointment[0].location}</td>
+                    <td>{appointment[0].dateOfAppointment}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+      {appointments.length === 0 && (
+        <div>
+          <h2>No appointment found!</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Appointment;
