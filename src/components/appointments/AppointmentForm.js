cf409cb3e946204ccb4e69251268bb5a73d0d96e
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-04-05 19:59:15 +0100

    Add create appointment

--- src/components/appointments/AppointmentForm.js
+++ src/components/appointments/AppointmentForm.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import DatePicker from 'react-date-picker';
+import AddAppointmentAPI from '../../api/addAppointmentAPI';
 
-const AppointmentForm = () => (
-  <h2>Welcome to appointment form homepage</h2>
-);
+const AppointmentForm = () => {
+  const doctors = useSelector((state) => state.doctors.doctors) || [];
+  const location = useLocation();
+  const [selectLocation, setSelectLocation] = useState('MaryLand');
+  const [selectDateOfAppointment, onChange] = useState(new Date());
+  const [loginResponse, setLoginResponse] = useState('');
+
+  const getDoctorId = () => {
+    if (location.state) {
+      return location.state.id;
+    }
+    if (doctors[0]) {
+      return doctors[0].id;
+    }
+    return 1;
+  };
+
+  const [doctorId, setDoctorId] = useState(getDoctorId());
+  const user = useSelector((state) => state.userReducer.user);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!user.id) {
+      setLoginResponse('Login to continue');
+    } else if (selectDateOfAppointment < new Date() || selectDateOfAppointment === new Date()) {
+      setLoginResponse('Date should be greater than today');
+    } else {
+      const response = await AddAppointmentAPI({
+        Location: selectLocation,
+        dateOfAppointment: selectDateOfAppointment,
+        user_id: user.id,
+        doctor_id: parseInt(doctorId, 10),
+      });
+      if (!response.error) {
+        setLoginResponse('Appointment was successfully created');
+      } else {
+        setLoginResponse(response.error);
+      }
+    }
+  };
+};
 
 export default AppointmentForm;
