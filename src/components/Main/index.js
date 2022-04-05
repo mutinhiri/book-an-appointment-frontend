f1a061f0a558f05dffa99d7b91d376b5917e2d61
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-05 19:10:46 +0200

    Initialize main page index

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import { BsCaretRightFill } from "react-icons/bs";
 import { BsFillCaretLeftFill } from "react-icons/bs";
-import Swiper from 'swiper/react'
+import Swiper from "swiper/react";
+import { SwiperSlide } from "swiper/react";
+import './Main.css'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import DoctorCard from "./DoctorCard";
+import 'swiper/css'
