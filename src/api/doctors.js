f6e76656f75cc48d05dd946c37c4f1ade69752fc
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-05 19:23:04 +0200

    Get doctors details from api

--- src/api/doctors.js
+++ src/api/doctors.js
@@ -8,4 +8,10 @@ import BaseUrl from "./BaseURL";
 export const getDoctors = async (dispatch) => {
   const response = await axios.get(`${BaseUrl}doctors`);
   dispatch(fetchDoctors(response.data));
+};
+
+export const getDoctorsDetail = (id) => async (dispatch) => {
+  fetch(`${BaseUrl}doctors/${id}`)
+    .then((res) => res.json())
+    .then((resResponse) => dispatch(fetchDetailDoctors(resResponse)));
 }
