72cf310edbe070003b09bf30c2ae12ad4e14908f
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-05 18:05:00 +0200

    Ad doctor reducers to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import doctorsReducer from './doctors/doctors';
+import doctorsDetailReducer from './doctors/DetailDoctors';
 import UserReducer from './users/users';
+import addItemReducer from './doctors/AddItem';
+import deleteItemReducer from './doctors/DeleteList';
 
 const reducers = combineReducers({
+  doctors: doctorsReducer,
+  doctorsDetailReducer,
   UserReducer,
+  addItemReducer,
+  deleteItemReducer,
 });
 
 export default createStore(reducers, applyMiddleware(thunk, logger));
