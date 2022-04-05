39f9b87ff8b6df133a4565fd0c8450be3c50a3b5
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-05 13:59:48 +0200

    Fix linter errors

--- src/components/Main/DoctorCard.js
+++ src/components/Main/DoctorCard.js
@@ -1,22 +1,22 @@
-import {BsTwitter, BsVimeo } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
-import PropTypes from 'prop-types'
+import { BsTwitter, BsVimeo } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
-const DoctorCard = ({doctor, onClick }) => {
+const DoctorCard = ({ doctor, onClick }) => {
   const {
     name, image, speciality,
   } = doctor;
   return (
-    <button type ="button" className='bg-transparent bordeless' onClick= {() => { onClick(); }}>
-      <div className='card borderless'>
-        <div className='card-image'>
+    <button type="button" className="bg-transparent bordeless" onClick={() => { onClick(); }}>
+      <div className="card borderless">
+        <div className="card-image">
           <img src={image} alt={name} className="doctor-card-image" />
         </div>
-        <div className='card-body'>
-          <h5 className='card-title'>{name}</h5>
-          <p className='card-text text-muted'>random description here</p>
-          <p className='card-text text-muted'>{speciality}</p>
-          <div className='rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted'>
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text text-muted">random description here</p>
+          <p className="card-text text-muted">{speciality}</p>
+          <div className="rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted">
             <FaFacebookF />
             <BsTwitter />
             <BsVimeo />
@@ -34,7 +34,7 @@ DoctorCard.PropTypes = {
     image: PropTypes.string.isRequired,
     speciality: PropTypes.string.isRequired,
   }).isRequired,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
 };
 
 export default DoctorCard;
