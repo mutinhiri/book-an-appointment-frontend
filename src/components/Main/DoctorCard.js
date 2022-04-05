5f6682b5732b5f8cbfd12bff34ad522c934db2ca
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-05 13:26:21 +0200

    Add doctor card image

--- src/components/Main/DoctorCard.js
+++ src/components/Main/DoctorCard.js
@@ -1,3 +1,18 @@
 import {BsTwitter, BsVimeo } from 'react-icons/bs'
 import { FaFacebookF } from 'react-icons/fa'
 import PropTypes from 'prop-types'
+
+const DoctorCard = ({doctor, onClick }) => {
+  const {
+    name, image, speciality,
+  } = doctor;
+  return (
+    <button type ="button" className='bg-transparent bordeless' onClick= {() => { onClick(); }}>
+      <div className='card borderless'>
+        <div className='card-image'>
+          <img src={image} alt={name} className="doctor-card-image" />
+        </div>
+      </div>
+    </button>
+  )
+}
