f43e9f77ae73796a9ed1d4293927c7db248b5aae
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-07 00:16:42 +0200

    Add image display

--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -6,4 +6,14 @@ import { BsFillGearFill } from "react-icons/bs";
 
 const DetailPage = () => {
   const doctor = useSelector((state) => state.doctorsDetailReducer.doctors) || [];
+  const navigate = useNavigate();
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col=md-8 col-12">
+          <img className="w=100 h-100" src={doctor.image} alt={doctor.name} />
+        </div>
+      </div>
+    </div>
+  )
 }
