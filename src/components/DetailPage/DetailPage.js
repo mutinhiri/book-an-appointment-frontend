00d05af0a7e47177b2b8f4ad633c7faa08fdea66
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-07 00:12:52 +0200

    Add doctors from state

--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -1,3 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useSelector } from "react-redux";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { BsFillGearFill } from "react-icons/bs";
+
+const DetailPage = () => {
+  const doctor = useSelector((state) => state.doctorsDetailReducer.doctors) || [];
+}
