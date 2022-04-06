ebb307977b460ce469ba227a30c285ecda840b67
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-06 09:32:09 +0200

    Main screen display changes

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,6 +7,7 @@ import './Main.css'
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DoctorCard from "./DoctorCard";
+import { getDoctorsDetail } from "../../api/doctors";
 import 'swiper/css'
 
 const Main = () => {
@@ -18,4 +19,25 @@ const Main = () => {
     setWidth(window.innerWidth)
   };
 
+  useEffect(() => {
+    window.addEventListener('resize', updateDimensions);
+
+    return () => window.removeEventListener('resize', updateDimensions)
+  }, [doctors])
+
+  const navigate = useNavigate();
+
+  const GoToDetailPage = (id) => {
+    dispatch(getDoctorsDetail(id)).then(() => {
+      navigate(`/Detail/${id}`);
+    });
+  };
+
+  return (
+    <div>
+      <h1 className="fw-bolder text-center">AVAILABLE DOCTORS</h1>
+      <p className="text-muted text-center main-screen-subtitle">Please select doctor specialist</p>
+      
+    </div>
+  )
 }
