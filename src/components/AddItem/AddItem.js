a6536833186c93f2591a4d41266ad3776cf4fe48
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-07 13:06:57 +0200

    Add items to database

---
+++ src/components/AddItem/AddItem.js
@@ -0,0 +1,36 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { AddItemHandler } from "../../api/doctors";
+import { getDoctors } from "../../api/doctors";
+
+const AddItem = () => {
+  const dispatch = useDispatch();
+  const {
+    register, handleSubmit, reset, } = useForm();
+  const [isItemAdded, setIsItemAdded] = useState(null);
+  const [errorResponse, setErrorResponse] = useState(null);
+
+  const onSubmit = async (data) => {
+    const response = await AddItemHandler(dispatch, data);
+    if (response.status === 201) {
+      setIsItemAdded(true);
+      reset();
+      dispatch(getDoctors);
+    } else {
+      setErrorResponse(JSON.stringify(response.data))
+    }
+  };
+
+  return (
+    <div className="row">
+      <div className="p-0 d-flex flex-column justify-content-center align-items-center">
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-5 mx-3">
+          <input type="text" className="form-control m-1" id="name" placeholder="Enter Name" {...register('name')} />
+          <textarea rows="4" cols="50" type="text" className="form-control m-1" id="description" />
+        </form>
+      </div>
+    </div>
+  )
+
+}
