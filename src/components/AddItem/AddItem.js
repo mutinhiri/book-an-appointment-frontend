eb82d9dbfa2ac49636ef6ec70be6dc10f55dae71
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-07 13:09:44 +0200

    Add image to form control

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -27,7 +27,8 @@ const AddItem = () => {
       <div className="p-0 d-flex flex-column justify-content-center align-items-center">
         <form onSubmit={handleSubmit(onSubmit)} className="mt-5 mx-3">
           <input type="text" className="form-control m-1" id="name" placeholder="Enter Name" {...register('name')} />
-          <textarea rows="4" cols="50" type="text" className="form-control m-1" id="description" />
+          <textarea rows="4" cols="50" type="text" className="form-control m-1" id="bio" placeholder="Enter Bio here" {...register('specialty')} />
+          <input type="text"  /> 
         </form>
       </div>
     </div>
