a0e177987d879c37f8d1d34b507473d8c5cd4f86
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-08 13:02:23 +0200

    Add doctor card

--- src/components/Main/DoctorCard.js
+++ src/components/Main/DoctorCard.js
@@ -10,19 +10,21 @@ const DoctorCard = ({ doctor, onClick }) => {
     <button type="button" className="bg-transparent borderless" onClick={() => { onClick(); }}>
       <div className="card">
         <div className="">
-          <img src={image} alt="doctorimage" className="card-img-top" />
+          <img src={image} alt="doctorimage" className="card-img-top doctor-card-image" />
         </div>
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <p className="card-text text-muted">{bio}</p>
-          <div className="rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted">
-            <FaFacebookF />
-          </div>
-          <div className="rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted">
-            <BsTwitter />
-          </div>
-          <div className="rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted">
-            <BsVimeo />
+          <div className="card-text d-flex justify-content-center">
+            <div className="rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted">
+              <FaFacebookF />
+            </div>
+            <div className="rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted">
+              <BsTwitter />
+            </div>
+            <div className="rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted">
+              <BsVimeo />
+            </div>
           </div>
         </div>
       </div>
