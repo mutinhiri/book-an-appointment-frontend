213913e512720567f6c060b126a144a4e15e9314
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-08 21:55:17 +0200

    Add changes

--- src/api/BaseURL.js
+++ src/api/BaseURL.js
@@ -1,6 +1,6 @@
-const Development = 'http://127.0.0.1:8080/api/v1/';
-// const Production = '';
+// const Development = 'http://127.0.0.1:8080/api/v1/';
+const Production = 'https://salty-atoll-87762.herokuapp.com/api/v1/';
 
-const BaseUrl = Development;
+const BaseUrl = Production;
 
 export default BaseUrl;
