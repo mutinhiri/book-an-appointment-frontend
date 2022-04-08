440a6b9c32f913b15c79960ef63b8a682521bc09
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-08 12:24:07 +0200

    Add changes

--- src/components/Main/DoctorCard.js
+++ src/components/Main/DoctorCard.js
@@ -10,11 +10,10 @@ const DoctorCard = ({ doctor, onClick }) => {
     <button type="button" className="bg-transparent bordeless" onClick={() => { onClick(); }}>
       <div className="card borderless">
         <div className="card-image">
-          <img src={image} alt="doctorimage" className="doctor-card-image" />
+          <img src={image} alt="doctorimage" className="doctor-card-image rounded-circle" />
         </div>
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
-          <p className="card-text text-muted">random description here</p>
           <p className="card-text text-muted">{bio}</p>
           <div className="rounded-circle border border-muted border-2 card-icon-circle justify-content-center align-items-center d-flex mx-1 text-muted">
             <FaFacebookF />
